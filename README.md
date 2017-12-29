c1c1b3366095ca27e084fc38d9be41f89b5c7d42
Joo <000null000@gmx.de>
2017-12-29 22:20:01 +0100

    updated comments

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # PixelFlutHexSnakes
 Snake like patterns moving in a hexagonal pattern for defnull's [Pixelflut](https://github.com/defnull/pixelflut)
 
+Python 2.7.11
+
+Serverrestart doesn't work
+
 <p align="center">
 <img align="center" src="hexsnake.gif" alt="Hexsnake animation"/>
 </p>
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+# 171229 joo
+# github.com/a9
+# thingiverse.com/joo
+#
+# hexsnakes for pixelflut
+# 34C3
+#
+# code base from standard python example for pixelflut
+# python 2.7.11
+
 import socket
 import time
 import random
@@ -7,8 +17,6 @@ PORT = 8080
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
 send = sock.send
-
-
 
 def pixel(x, y, r, g, b, a=255):
     global sock
@@ -20,6 +28,7 @@ def pixel(x, y, r, g, b, a=255):
             send('PX %d %d %02x%02x%02x%02x\n' % (x, y, r, g, b, a))
             #print('PX %d %d %02x%02x%02x%02x\n' % (x, y, r, g, b, a))
     except:
+        # Serverrestart doesn't work right now
         print("new socket")
         sock.shutdown(1)
         sock.close()
@@ -27,7 +36,6 @@ def pixel(x, y, r, g, b, a=255):
 
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((HOST, PORT))
-
 
 def rect(x,y,w,h,r,g,b):
   for i in xrange(x,x+w):
@@ -37,10 +45,7 @@ def rect(x,y,w,h,r,g,b):
 def clamp(n, minn, maxn):
     return max(min(maxn, n), minn)
 
-
-
 class Hexsnake():
-
     def __init__(self):
         self.x = 400
         self.y = 200
@@ -51,9 +56,7 @@ class Hexsnake():
         self.green  = random.random()*255
         self.blue  = random.random()*255
 
-
     def run(self):
-
         offsx = [0, 2,  2, 0, -2, -2]
         offsy = [3, 1, -1, -3, -1, 1]
 
@@ -68,7 +71,6 @@ class Hexsnake():
 
             rect(self.x, self.y, self.width, self.width/2, self.red, self.green, self.blue)
             rect(self.x, self.y+self.width/2, self.width, self.width/2, self.red/2, self.green/2, self.blue/2)
-
 
 hexsnake = Hexsnake()
 
